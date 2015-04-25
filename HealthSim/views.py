1607f9eb5ed717c792f6d963bac97d41caad6ddb
Jeffrey DiMarco <jdimarco218@gmail.com>
2015-04-24 23:46:36 -0400

    Receive data from front end, send to run simulation, send results back to front end

--- HealthSim/views.py
+++ HealthSim/views.py
@@ -20,39 +20,16 @@ class HealthSimView(TemplateView):
 def ajax_view_0(request):
 
     if request.method == "POST":
-        print("Checking..")
-        if 'client_ajax_input_0' in request.POST:
-            print("Have dummY")
-        else:
-            print("no dummy")
-        if 'client_input_dict' in request.POST:
-            print("Hurray!")
-        else:
-            print("No data :(")
-        tempList = request.POST.getlist('client_input_dict')
-        print("array:")
-        print(tempList[0])
-
         # Get inputs
-        try:
-            response_list = [] 
-            response_list = request.POST.getlist('client_input_dict')
-            simulation_input = {"me" : json.loads(response_list[0])}
-            # Run simulation if we have input
-            simulation_result = {}
-            if simulation_input:
-                print("Simulation input:",simulation_input)
-                simulation_result = run_simulations(simulation_input)
-        except Exception:
-            print('\n'.join(traceback.format_exception(*sys.exc_info())))
-            raise
+        response_list = [] 
+        response_list = request.POST.getlist('client_input_dict')
+        simulation_input = {"me" : json.loads(response_list[0])}
+        # Run simulation if we have input
+        simulation_result = {}
+        if simulation_input:
+            simulation_result = run_simulations(simulation_input)
 
-        # Send results 
-        response_dict = {}
-        for entry in simulation_result:
-            response_dict.update(entry)
-        #response_dict.update({'response_item_0': "I am item 0 :)", 'response_item_1' : "I am item 1 and I'm a loser :("})
-        return HttpResponse(json.dumps(response_dict), content_type='application/json')
+        return HttpResponse(json.dumps(simulation_result), content_type='application/json')
     else:
         raise Http404("IDK LOL")
 
