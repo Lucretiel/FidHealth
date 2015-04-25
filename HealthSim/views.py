7652a317f4988b9112753d21dc95de4d56e36e1d
Jeffrey DiMarco <jdimarco218@gmail.com>
2015-04-24 22:55:55 -0400

    making dev branch

--- HealthSim/views.py
+++ HealthSim/views.py
@@ -1,3 +1,5 @@
+import traceback
+import sys
 import json
 import socket
 
@@ -7,6 +9,7 @@ from django.http import HttpResponse, Http404
 from django.views.generic import TemplateView
 
 from .insurance import plans as plans
+from .insurance.plans import run_simulations
 
 plans.load_plans()
 
@@ -16,9 +19,39 @@ class HealthSimView(TemplateView):
 
 def ajax_view_0(request):
 
-    if 'client_ajax_input_0' in request.POST:
+    if request.method == "POST":
+        print("Checking..")
+        if 'client_ajax_input_0' in request.POST:
+            print("Have dummY")
+        else:
+            print("no dummy")
+        if 'client_input_dict' in request.POST:
+            print("Hurray!")
+        else:
+            print("No data :(")
+        tempList = request.POST.getlist('client_input_dict')
+        print("array:")
+        print(tempList[0])
+
+        # Get inputs
+        try:
+            response_list = [] 
+            response_list = request.POST.getlist('client_input_dict')
+            simulation_input = {"me" : json.loads(response_list[0])}
+            # Run simulation if we have input
+            simulation_result = {}
+            if simulation_input:
+                print("Simulation input:",simulation_input)
+                simulation_result = run_simulations(simulation_input)
+        except Exception:
+            print('\n'.join(traceback.format_exception(*sys.exc_info())))
+            raise
+
+        # Send results 
         response_dict = {}
-        response_dict.update({'response_item_0': "I am item 0 :)", 'response_item_1' : "I am item 1 and I'm a loser :("})
+        for entry in simulation_result:
+            response_dict.update(entry)
+        #response_dict.update({'response_item_0': "I am item 0 :)", 'response_item_1' : "I am item 1 and I'm a loser :("})
         return HttpResponse(json.dumps(response_dict), content_type='application/json')
     else:
         raise Http404("IDK LOL")
